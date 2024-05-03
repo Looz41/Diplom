53d53374e0798404cc5844670bd116f23b4afe93
Looz <Shirabokovi99@gmail.com>
2024-05-04 02:29:36 +0300

    fix ts

--- middlewares/roleMiddleware/index.ts
+++ middlewares/roleMiddleware/index.ts
@@ -2,37 +2,33 @@ import { NextFunction, Request, Response } from "express";
 import { SecretKey } from "../../config";
 const jwt = require('jsonwebtoken')
 
-/**
- * Функция ограничения прав доступа по Роли
- * @param roles Массив строк, где строка это Роль
- * @returns Права доступа
- */
 export const roleMiddleware = (roles: Array<string>) => {
     return function (req: Request, res: Response, next: NextFunction) {
         if (req.method === "OPTIONS") {
-            next()
+            next();
+            return;
         }
 
         try {
-            const token = req.headers.authorization.split(' ')[1]
+            const token = req.headers.authorization?.split(' ')[1]; // Проверяем, что headers не undefined
             if (!token) {
-                return res.status(403).json({ message: 'Пользователь не авторизован' })
-
+                return res.status(403).json({ message: 'Пользователь не авторизован' });
             }
-            const { roles: userRoles } = jwt.verify(token, SecretKey.secret)
-            let hasRole: boolean = false
-            userRoles.forEach(role => {
+
+            const { roles: userRoles } = jwt.verify(token, SecretKey.secret) as { roles: string[] }; // Указываем тип для roles
+            let hasRole = false;
+            userRoles.forEach((role: string) => {
                 if (roles.includes(role)) {
-                    hasRole = true
+                    hasRole = true;
                 }
             });
             if (!hasRole) {
-                return res.status(403).json({ message: "У вас нет доступа" })
+                return res.status(403).json({ message: "У вас нет доступа" });
             }
-            next()
+            next();
         } catch (e) {
             console.log(e);
-            return res.status(403).json({ message: 'Пользователь не авторизован' })
+            return res.status(403).json({ message: 'Пользователь не авторизован' });
         }
-    }
+    };
 };
