f0398e90c1a6ea8d37bff55ad4349f3d36b85c0b
Looz <Shirabokovi99@gmail.com>
2024-05-04 02:32:58 +0300

    fix ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { authRouter } from './Routes/authRouter';
 import { disciplineRouter } from './Routes/sheduleHelping/disciplineRouter';
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Middleware для отключения CORS (временно!)
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
