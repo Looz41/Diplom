c5a52dbc4033c6ac142100301b9c1776bfb36415
Looz <Shirabokovi99@gmail.com>
2024-05-05 20:35:45 +0300

    fix

--- Routes/authRouter/index.ts
+++ Routes/authRouter/index.ts
@@ -6,7 +6,7 @@ const authRouter = express.Router();
 const controller = new authController();
 
 
-authRouter.post('/registration', registrationValidationRules, controller.registration);
+authRouter.post('/registration', controller.registration);
 authRouter.post('/login', controller.login);
 authRouter.get('/users', roleMiddleware(['ADMIN']), controller.getUsers);
 authRouter.get('/addRole', controller.addRole);
