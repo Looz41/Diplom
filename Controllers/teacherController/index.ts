7edff24617928e6842161a964b86917334f18c87
Looz <Shirabokovi99@gmail.com>
2024-05-12 04:29:32 +0300

    fix

--- Controllers/teacherController/index.ts
+++ Controllers/teacherController/index.ts
@@ -271,29 +271,29 @@ class teachersController {
     async getTeacherByDiscipline(req: Request, res: Response) {
         try {
             const { id, date } = req.query;
-    
+
             if (!req.query || !id) {
                 return res.status(400).json({ message: 'Идентификатор дисциплины не указан' });
             }
-    
+
             if (!mongoose.Types.ObjectId.isValid(id.toString())) {
                 return res.status(400).json({ message: 'Неверный формат id' })
             }
-    
+
             const discipline = await Disciplines.findOne({ _id: id })
                 .populate('teachers')
                 .exec();
-    
+
             if (!discipline) {
                 return res.status(404).json({ message: 'Дисциплина не найдена' });
             }
-    
+
             const teachers: (typeof Teachers & { aH: number, burden: { hH?: number; mounth?: Date; }[] })[] = (discipline.teachers as unknown) as (typeof Teachers & { aH: number, burden: { hH?: number; mounth?: Date; }[] })[];
-    
+
             const teachersWithHH = teachers.filter(teacher => {
                 const filtered = teacher.burden.filter(e => {
-                    const burdenDate = new Date(e.mounth);
-                    const queryDate = new Date(date as string);
+                    const burdenDate = e.mounth ? new Date(e.mounth) : undefined;
+                    const queryDate = date ? new Date(date as string) : undefined;
                     return (
                         burdenDate.getMonth() === queryDate.getMonth() &&
                         burdenDate.getFullYear() === queryDate.getFullYear()
@@ -301,7 +301,7 @@ class teachersController {
                 });
                 return filtered.length > 0 && filtered[0].hH !== undefined && filtered[0].hH !== 0;
             });
-    
+
             const teachersWithoutHH = teachers.filter(teacher => {
                 const filtered = teacher.burden.filter(e => {
                     const burdenDate = new Date(e.mounth);
@@ -313,7 +313,7 @@ class teachersController {
                 });
                 return filtered.length === 0 || filtered[0].hH === undefined || filtered[0].hH === 0;
             });
-    
+
             teachersWithHH.sort((a, b) => {
                 const bHH = b.burden.filter((e: any) => {
                     const burdenDate = new Date(e.mounth);
@@ -323,11 +323,11 @@ class teachersController {
                         burdenDate.getFullYear() === queryDate.getFullYear()
                     );
                 }).reduce((acc: any, cur: any) => acc + cur.hH, 0);
-    
+
                 const aHH = a.burden.reduce((acc: any, cur: any) => acc + cur.hH, 0);
                 return (b.aH / bHH) - (a.aH / aHH);
             });
-    
+
             res.json({ teachers: [...teachersWithoutHH, ...teachersWithHH] });
         } catch (error) {
             console.error(error);
