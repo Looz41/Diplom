a1aaed752aa41b7d6a10752709e4b93df1635c9a
Looz <Shirabokovi99@gmail.com>
2024-06-04 01:28:23 +0300

    fix

--- Controllers/scheduleController/index.ts
+++ Controllers/scheduleController/index.ts
@@ -763,7 +763,7 @@ class scheduleController {
             // Calculate the number of days in the specified month
             const daysInMonth = new Date(year, month, 0).getDate();
 
-            for (let day = 1; day <= daysInMonth; day++) {
+            for (let day = 1; day <= 3; day++) {
                 const date = new Date(year, month - 1, day); // month is zero-based in JS Date
 
                 for (const group of groups) {
